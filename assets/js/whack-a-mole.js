043ed667e68e6a1efd58145596b935ba691af27f
KoroYsabela <ysabelabathan2604@gmail.com>
2025-09-17 13:08:50 +0100

    add: mole css class that will appear on tiles

--- assets/js/whack-a-mole.js
+++ assets/js/whack-a-mole.js
@@ -2,9 +2,22 @@
 const gridSize = 9;
 
 
+// Let
+let score = 0;
+let moveMoleInterval = 2000; // milliseconds = 2 seconds
+let gameDuration = 30000; // milliseconds = 30 seconds
 
+// Selectors
+const startBtn = document.getElementById("start-btn");
+let scoreText = document.getElementById("score-text");
 
 
 function getRandomTile() {
     return Math.floor(Math.random() * gridSize);
 }
+
+
+
+// function placeMole() { 
+
+// }
