9efb221a3c6cb912dc426d9f3791f183a37ccc84
KoroYsabela <ysabelabathan2604@gmail.com>
2025-09-17 14:18:09 +0100

    add: mole moving to different tiles on set interval

--- assets/js/whack-a-mole.js
+++ assets/js/whack-a-mole.js
@@ -4,20 +4,83 @@ const gridSize = 9;
 
 // Let
 let score = 0;
-let moveMoleInterval = 2000; // milliseconds = 2 seconds
-let gameDuration = 30000; // milliseconds = 30 seconds
+let moveMoleInterval = 2; // seconds = 2000 milliseconds
+let gameDuration = 30; // seconds = 30000 milliseconds
+// let previousTile; // make sure that the mole always moves to a new tile
 
 // Selectors
 const startBtn = document.getElementById("start-btn");
 let scoreText = document.getElementById("score-text");
+let timeLeftText = document.getElementById("time-left");
+const gridTiles = document.querySelectorAll(".tile");
 
 
-function getRandomTile() {
-    return Math.floor(Math.random() * gridSize);
+// Event Listeners
+startBtn.addEventListener('click', startGame);
+
+// Functions
+function startGame() {
+    score = 0;
+    console.log("Game Started");
+    // startTimer();
+    moveMole();
+    
 }
 
 
+function startTimer () {
+    timeLeftText.innerText = gameDuration.toString();
+}
 
-// function placeMole() { 
 
+function moveMole() {   
+    let moleTimer = null;
+
+    let moleInterval = convertToMilliseconds(moveMoleInterval);
+
+    // console.log(moleInterval);
+
+    moleTimer = setInterval(getRandomTile, moleInterval);    
+}
+
+// function removeMole(tile) {
+//     tile.classList.remove('mole');
+// }
+
+// Loop through grids with class "tile", 
+// pick a random tile for the mole css class to be added
+function getRandomTile() {
+    console.log("moving mole");
+
+    const chosenTile = getRandomNumber(gridSize);
+
+    // Remove any existing mole class in tiles
+    for (const tile of gridTiles) {
+        tile.classList.remove('mole');
+    }
+    gridTiles[chosenTile].classList.add('mole');
+
+
+    // for (let i = 0; i < gridSize; i++)
+    // {
+    //     if(i === chosenTile)
+    //     {
+    //         gridTiles[i].classList.add('mole');
+    //         break;
+    //     }
+
+    // }
+
+}
+
+function getRandomNumber(maxNumber) {
+    return Math.floor(Math.random() * maxNumber);
+}
+
+function convertToMilliseconds(seconds) {
+    return seconds * 1000;
+}
+
+// function convertToSeconds(milliseconds) {
+//     return milliseconds / 1000;
 // }
