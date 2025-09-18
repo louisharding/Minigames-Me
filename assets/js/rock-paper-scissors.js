411a5e83cb3f2d2bc426360af10e32c5d2159a4f
ETCocoa <adamlh2003@gmail.com>
2025-09-18 12:38:22 +0100

    Add last of main game code

--- assets/js/rock-paper-scissors.js
+++ assets/js/rock-paper-scissors.js
@@ -1,49 +1,65 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("choice-rock");
 const paper_div = document.getElementById("choice-paper");
 const scissors_div = document.getElementById("choice-scissors");
 
 function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
+  const choices = ["Rock", "Paper", "Scissors"];
   const randomNumber = Math.floor(Math.random() * 3);
   return choices[randomNumber];
+}
+
+function win(userChoice, computerChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  result_p.innerHTML = `Your ${userChoice} beats the AI's ${computerChoice}. You win!`;
+}
+
+function lose(userChoice, computerChoice) {
+  computerScore++;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `The AI's ${computerChoice} beats your ${userChoice}. You lose!`;
+}
+
+function draw(userChoice) {
+  result_p.innerHTML = `Both you and the AI chose ${userChoice}. It's a draw!`;
 }
 
 function game(userChoice) {
   const computerChoice = getComputerChoice();
   switch (userChoice + computerChoice) {
-    case "rockscissors":
-    case "paperrock":
-    case "scissorspaper":
-      console.log("User wins");
+    case "RockScissors":
+    case "PaperRock":
+    case "ScissorsPaper":
+      win(userChoice, computerChoice);
       break;
-    case "rockpaper":
-    case "paperscissors":
-    case "scissorsrock":
-      console.log("Computer wins");
+    case "RockPaper":
+    case "PaperScissors":
+    case "ScissorsRock":
+      lose(userChoice, computerChoice);
       break;
     default:
-      console.log("It's a draw");
+      draw(userChoice);
       break;
   }
 }
 
 function main() {
   rock_div.addEventListener("click", function () {
-    game("rock");
+    game("Rock");
   });
 
   paper_div.addEventListener("click", function () {
-    game("paper");
+    game("Paper");
   });
 
   scissors_div.addEventListener("click", function () {
-    game("scissors");
+    game("Scissors");
   });
 }
 
